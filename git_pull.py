bd42215b7773242fe9f4a07057487848cc01fcb6
makoto ohno <mo@takmi.jp>
2019-03-15 07:25:02 +0900

    from:mo@cent7-takmi-jobs

--- git_pull.py
+++ git_pull.py
@@ -2,9 +2,8 @@
 # coding: utf-8
 
 
-# https://qiita.com/tdrk/items/9b23ad6a58ac4032bb3b
-
 import os, sys, subprocess, datetime, socket
+
 
 # https://note.nkmk.me/python-os-environ-getenv/
 result_env = os.getenv('SSH_ASKPASS')
@@ -12,6 +11,7 @@ if result_env != None:
     del os.environ['SSH_ASKPASS']
 print('SSH_ASKPASS : ' + str(os.getenv('SSH_ASKPASS')))
 
-cmd_pull_azu="git pull azure_ssh master"
-subprocess.call( cmd_pull_azu, shell=True )
+
+cmd_pull_org="git pull origin master"
+subprocess.call( cmd_pull_org, shell=True )
 
