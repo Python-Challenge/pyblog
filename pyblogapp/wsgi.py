81b2db202da76d76d0ffa09aeb872d2c8977483c
makoto ohno <mo@takmi.jp>
2019-03-17 18:16:37 +0900

    static 寄せと自動起動 他...

--- pyblogapp/wsgi.py
+++ pyblogapp/wsgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyblogapp.settings')
 
 application = get_wsgi_application()
+
+'''
+# add : https://qiita.com/waffle/items/835625c2413ff6a387c6
+def application(environ, start_response):
+    try:
+        body = {k + ': ' + str(v) for k, v in environ.items()}
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return ['\n'.join(body).encode()]
+    except:
+        import traceback
+        start_response('500 Internal Server Error', [('Content-type', 'text/plain')])
+        return [traceback.format_exc().encode()]
+'''
